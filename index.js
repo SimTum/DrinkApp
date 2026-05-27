3447d3bae7b2d5c75f64a8847af8073d96cb2a7d
Simon Tumanov <Simon.tum@outlook.com>
2026-05-27 20:15:35 -0300

    removed +

--- index.js
+++ index.js
@@ -1,85 +1,76 @@
-require('dotenv').config()
-import express, { response } from 'express'
-import mongoose from 'mongoose'
-import Beverage from './models/beverage.model.js'
-import Ingredient from './models/ingredient.model.js'
-import Bevarage from './models/beverage.model.js'
+require("dotenv").config();
+import express, { response } from "express";
+import mongoose from "mongoose";
+import Beverage from "./models/beverage.model.js";
+import Ingredient from "./models/ingredient.model.js";
+import Bevarage from "./models/beverage.model.js";
 
+const app = express();
+app.use(express.json());
 
-const app = express()
-app.use(express.json())
+const mongoUrl = process.env.MONGO_CONNECTION_STRING;
 
-const mongoUrl = process.env.MONGO_CONNECTION_STRING
- 
-mongoose.connect(mongoUrl)
-    .then(() => console.log("Successful connected to MongoDB"))
-    .catch((err) => console.log("Connection failed", err)
-    )
+mongoose
+  .connect(mongoUrl)
+  .then(() => console.log("Successful connected to MongoDB"))
+  .catch((err) => console.log("Connection failed", err));
 
-app.get('/', (req, res) => {
-    res.send('Hello World, We are testing if it is actually updating')
-})
-
-app.get('/api/beverages', async (req, res) => {
-    try {
-        const bevarages = await Beverage.find({});
-        res.status(200).json(bevarages);
-    } catch (error) {
-        res.status(500).json({ message: error })
-    }
-})
-
-app.get('/api/beverage/:id', async (req, res) => {
-    try {
-        const { id } = req.params;
-        const bevarage = await Beverage.findById(id);
-        res.status(200).json(bevarage);
-    } catch (error) {
-        res.status(500).json({ message: error })
-    }
-})
-
-
-
-
-app.post('/api/beverage', async (req, res) => {
-    try {
-        const beverage = await Beverage.create(req.body)
-        res.status(200).json(beverage)
-
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-
-
+app.get("/", (req, res) => {
+  res.send("Hello World, We are testing if it is actually updating");
 });
 
-app.put('api/beverage/:id', async (req, res) => {
+app.get("/api/beverages", async (req, res) => {
+  try {
+    const bevarages = await Beverage.find({});
+    res.status(200).json(bevarages);
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
+});
+
+app.get("/api/beverage/:id", async (req, res) => {
+  try {
     const { id } = req.params;
-    try {
-        const beverage = await Bevarage.findByIdAndUpdate(id, req.body);
-        if (!beverage) {
-            return res.status(400).json({ message: "bebida não foi encontrada" })
-        }
+    const bevarage = await Beverage.findById(id);
+    res.status(200).json(bevarage);
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
+});
 
-        const updBeverage = await Beverage.findById(id)
-        res.status(200).json({ message: updBeverage });
-    }
-    catch (err) {
-        res.status(500).json({ message: err });
-    }
-})
+app.post("/api/beverage", async (req, res) => {
+  try {
+    const beverage = await Beverage.create(req.body);
+    res.status(200).json(beverage);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
-app.delete('api/beverage/:id', async (req, res) => {
-    const { id } = req.params;
-    try {
-        const beverage = await Beverage.findByIdAndDelete(id);
-        res.status(200)
+app.put("api/beverage/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const beverage = await Bevarage.findByIdAndUpdate(id, req.body);
+    if (!beverage) {
+      return res.status(400).json({ message: "bebida não foi encontrada" });
     }
-    catch (error) {
-        res.status(500).json({ message: error })
-    }
-})
+
+    const updBeverage = await Beverage.findById(id);
+    res.status(200).json({ message: updBeverage });
+  } catch (err) {
+    res.status(500).json({ message: err });
+  }
+});
+
+app.delete("api/beverage/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const beverage = await Beverage.findByIdAndDelete(id);
+    res.status(200);
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
+});
 
 // INGREDIENTS
 // INGREDIENTS
@@ -98,23 +89,15 @@ app.delete('api/beverage/:id', async (req, res) => {
 // INGREDIENTS
 // INGREDIENTS
 
-app.get('/api/ingredients', async (req,res) => {
-    try {
-        const ingredients = await Ingredient.find({})
-        res.status(200).json(ingredients)
-        
-    } catch (error) {
-        res.status(500).json({messag: error})
-    }
-})
-
-app.
+app.get("/api/ingredients", async (req, res) => {
+  try {
+    const ingredients = await Ingredient.find({});
+    res.status(200).json(ingredients);
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
+});
 
 app.listen(3000, () => {
-+
-    console.log("Hello, woof! We are at http://localhost:3000");
-
-})
-
-
-
+  console.log("Hello, woof! We are at http://localhost:3000");
+});
